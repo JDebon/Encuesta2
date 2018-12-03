4188eaf5b9a7a5254969b23067631ee9a1912c55
John-Debon <matudbonis@gmail.com>
2018-12-03 20:43:35 -0300

    Guia 1 completed

--- js/controlador/controlador.js
+++ js/controlador/controlador.js
@@ -2,11 +2,14 @@
  * Controlador
  */
 var Controlador = function(modelo) {
-  this.modelo = modelo;
+    this.modelo = modelo;
 };
 
 Controlador.prototype = {
-  agregarPregunta: function(pregunta, respuestas) {
-      this.modelo.agregarPregunta(pregunta, respuestas);
-  },
+    agregarPregunta: function(pregunta, respuestas) {
+        this.modelo.agregarPregunta(pregunta, respuestas);
+    },
+    borrarPregunta: function(id) {
+        this.modelo.borrarPregunta(id);
+    }
 };
--- js/modelo/modelo.js
+++ js/modelo/modelo.js
@@ -2,29 +2,47 @@
  * Modelo
  */
 var Modelo = function() {
-  this.preguntas = [];
-  this.ultimoId = 0;
+    this.preguntas = [];
+    this.ultimoId = 0;
 
-  //inicializacion de eventos
-  this.preguntaAgregada = new Evento(this);
+    //inicializacion de eventos
+    this.preguntaAgregada = new Evento(this);
+    this.preguntaEliminada = new Evento(this);
 };
 
 Modelo.prototype = {
-  //se obtiene el id más grande asignado a una pregunta
-  obtenerUltimoId: function() {
-  },
+    //se obtiene el id más grande asignado a una pregunta
+    obtenerUltimoId: function() {
+        let mayor = 0;
+        this.preguntas.forEach(function(element) {
+            if (element.id > mayor) {
+                mayor = element.id;
+            }
+        });
+        console.log(mayor);
+        return mayor;
+    },
 
-  //se agrega una pregunta dado un nombre y sus respuestas
-  agregarPregunta: function(nombre, respuestas) {
-    var id = this.obtenerUltimoId();
-    id++;
-    var nuevaPregunta = {'textoPregunta': nombre, 'id': id, 'cantidadPorRespuesta': respuestas};
-    this.preguntas.push(nuevaPregunta);
-    this.guardar();
-    this.preguntaAgregada.notificar();
-  },
+    //se agrega una pregunta dado un nombre y sus respuestas
+    agregarPregunta: function(nombre, respuestas) {
+        var id = this.obtenerUltimoId();
+        id++;
+        var nuevaPregunta = { textoPregunta: nombre, id: id, cantidadPorRespuesta: respuestas };
+        this.preguntas.push(nuevaPregunta);
+        this.guardar();
+        this.preguntaAgregada.notificar();
+    },
+    borrarPregunta: function(id) {
+        this.preguntas = this.preguntas.filter(function(element) {
+            if (element.id !== id) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+        this.preguntaEliminada.notificar();
+    },
 
-  //se guardan las preguntas
-  guardar: function(){
-  },
+    //se guardan las preguntas
+    guardar: function() {}
 };
